58ec8ba6589c1e4d4083cde6266e475d41327170
Lanello33 <134654256+Lanello33@users.noreply.github.com>
2023-07-26 16:22:10 -0500

    Added bootstrap

---
+++ src/app/me/me.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pm-me',
+  templateUrl: './me.component.html',
+  styleUrls: ['./me.component.scss']
+})
+export class MeComponent {
+
+}
